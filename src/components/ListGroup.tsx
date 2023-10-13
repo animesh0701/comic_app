9714f197eba46d54d70c5a71a1119748772182c2
Animesh <87601469+animesh0701@users.noreply.github.com>
2023-10-13 15:11:25 +0530

    Rendered a List

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,13 @@
 function ListGroup() {
+  const comics = ["Nirvana", "Boundary", "JJK", "CSM", "OnePiece"];
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {comics.map((comic) => (
+          <li key={comic}>{comic}</li>
+        ))}
       </ul>
     </>
   );
