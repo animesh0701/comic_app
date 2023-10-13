28a2971994d600c6dbdc5bc52139897be6ccef34
Animesh <87601469+animesh0701@users.noreply.github.com>
2023-10-13 17:13:20 +0530

    Added Event Handler

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,20 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
-  let comics = ["Nirvana", "Boundary", "JJK", "CSM", "OnePiece"];
-  comics = [];
+  const comics = ["Nirvana", "Boundary", "JJK", "CSM", "OnePiece"];
+
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {comics.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {comics.map((comic) => (
-          <li key={comic}>{comic}</li>
+        {comics.map((comic, index) => (
+          <li className="list-group-item" key={comic} onClick={handleClick}>
+            {comic}
+          </li>
         ))}
       </ul>
     </>
