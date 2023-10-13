bee71307a44f3a14463e1be47e36202d75787a74
Animesh <87601469+animesh0701@users.noreply.github.com>
2023-10-14 01:55:31 +0530

    Passed function via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import ListGroup from "./components/ListGroup";
 const comics = ["Nirvana", "Boundary", "JJK", "CSM", "OnePiece"];
 
+const handleSelectItem = (item: string) => {
+  console.log(item);
+};
+
 function App() {
   return (
     <div>
-      <ListGroup items={comics} heading="Comics" />
+      <ListGroup
+        items={comics}
+        heading="Comics"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
