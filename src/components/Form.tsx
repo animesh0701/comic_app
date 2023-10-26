2b112b3db07bac4c4766fec24ababb608e15190d
Animesh <87601469+animesh0701@users.noreply.github.com>
2023-10-27 02:37:40 +0530

    fetching form data on submit as a object

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,34 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useRef } from "react";
 
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = {
+    name: "",
+    age: 0,
+  };
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Submitted");
+    if (nameRef.current !== null && ageRef.current !== null) {
+      person.name = nameRef.current.value;
+      person.age = parseInt(ageRef.current.value);
+      console.log(person);
+    }
   };
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
-        <label htmlFor="name" className="form-lable">
+        <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
         <button className="btn btn-primary" type="submit">
           Submit
         </button>
