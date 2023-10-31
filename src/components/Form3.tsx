c28f8383525d2cf6d3c15f7140c9dd7f96b60a02
Animesh <87601469+animesh0701@users.noreply.github.com>
2023-10-31 11:55:16 +0530

    Disabling submit button on invalid forms

--- src/components/Form3.tsx
+++ src/components/Form3.tsx
@@ -16,7 +16,7 @@ const Form3 = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
   const submitHandler = (data: FieldValues) => console.log(data);
   return (
@@ -44,7 +44,7 @@ const Form3 = () => {
           className="form-control"
         />
         {errors.age && <p className="text-danger">{errors.age.message}</p>}
-        <button className="btn btn-primary" type="submit">
+        <button disabled={!isValid} className="btn btn-primary" type="submit">
           Submit
         </button>
       </div>
