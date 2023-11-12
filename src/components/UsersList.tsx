f60d55b1936fb05b0b9205e4a05957a067227269
Animesh <87601469+animesh0701@users.noreply.github.com>
2023-11-12 16:19:03 +0530

    Handling errors

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -8,20 +8,25 @@ interface User {
 
 const UsersList = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
   }, []);
 
   return (
-    <ul>
-      <h1>Users List</h1>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        <h1>Users List</h1>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   );
 };
 
